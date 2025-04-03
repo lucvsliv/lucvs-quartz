6915e149366350cd364c6f8563d35c55f136fc38
gongjuheon <yh6543210@gmail.com>
2025-04-03 13:48:38 +0900

    chore: testing things

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Lucvs 🔬 Laboratory",
+    pageTitle: "Lucvs 🔬<br> Laboratory",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
