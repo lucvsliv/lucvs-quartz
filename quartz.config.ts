f1596e7e51f73206649ea2befd53f675d7d62874
gongjuheon <yh6543210@gmail.com>
2025-04-03 18:08:36 +0900

    chore: modify page title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Lucvs 🔬                Laboratory",
+    pageTitle: "Lucvs🔬" + "\n" + "Laboratory",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
