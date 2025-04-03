c6fb314694c0fb017848c120ff10ccdc77c87296
gongjuheon <yh6543210@gmail.com>
2025-04-03 19:52:47 +0900

    chore: change body font

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Gowun Dodum",
+        header: "Playfair Display",
         body: "Playfair Display",
         code: "JetBrains Mono",
       },
