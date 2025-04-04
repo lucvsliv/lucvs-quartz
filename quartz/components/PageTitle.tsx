3252bac214338f10b711245001fe92904ec99552
gongjuheon <yh6543210@gmail.com>
2025-04-04 12:49:27 +0900

    chore: change pate title font size

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -6,28 +6,30 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+
+  const handleClick = () => {
+    window.location.href = baseDir
+  }
+
   return (
     <div class={classNames(displayClass, "page-title")}>
-      <div class="page-title">
-        <a href={baseDir}>{title}</a>
+      <div class="page-title-text" onClick={handleClick}>
+        {title}
       </div>
     </div>
   )
 }
 
 PageTitle.css = `
-.page-title {
-  font-family: "Quintessential"
+.page-title-text {
+  font-family: "Quintessential";
   font-weight: bold;
   color: var(--dark);
   font-size: 3.30rem;
   margin: 0;
-}
-.page-title a {
+  cursor: pointer;
   text-decoration: none;
-  color: inherit;
 }
 `
-
 
 export default (() => PageTitle) satisfies QuartzComponentConstructor
